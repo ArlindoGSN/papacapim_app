7d9e7042a1a1634e15c0a45990022d33cf32375b
Arlindo Neto <netonery06@gmail.com>
2025-02-08 12:20:36 -0300

    feat(page):add splash page

---
+++ lib/commons/constants/app_colors.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color primary = Colors.black;
+  static const Color secondary = Colors.yellow;
+  static const Color third = Colors.yellowAccent;
+  static const Color background = Colors.white;
+}
---
+++ lib/features/splash/splash_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:papacapim_app/commons/constants/app_colors.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container( 
+        alignment: Alignment.center,
+        
+        decoration: BoxDecoration(
+          
+          gradient: LinearGradient(colors: [AppColors.secondary, AppColors.third], begin: Alignment.topCenter, end: Alignment.bottomCenter),
+        ),
+        child: const Text('Papacapim App', style: TextStyle(color: AppColors.primary, fontSize: 50, fontWeight: FontWeight.bold),),
+      ),
+    );
+  }
+}
