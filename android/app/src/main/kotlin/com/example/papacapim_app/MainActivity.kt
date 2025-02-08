82b0be9e5fe145484d0e7c4700bdc0523ce99a0f
Arlindo Neto <netonery06@gmail.com>
2025-02-08 11:12:42 -0300

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/papacapim_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.papacapim_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:papacapim_app/app.dart';
+
+void main() {
+  runApp(const App());
+}
+
